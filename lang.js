f2dfe0e04f2c0932de7eb2de9e1a998c9db35f28
MrBensor <fortfandel@gmail.com>
2025-09-12 19:15:49 +0200

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,16 @@
+lconst translations = {
+    de: {
+        title: "CS2 Float Rechner",
+        tab1: "Float berechnen",
+        tab2: "Maximaler Durchschnitt",
+        searchPlaceholder: "Skin suchen...",
+        // ...weitere Schlüssel
+    },
+    en: {
+        title: "CS2 Float Calculator",
+        tab1: "Calculate Float",
+        tab2: "Max Average",
+        searchPlaceholder: "Search skin...",
+        // ...weitere Schlüssel
+    }
+};
